d83f8489023a33a4de71d5dbfc36be3309a59841
Irvin Ruiz <ruizirvin084@gmail.com>
2021-12-08 12:00:19 -0600

    Added js file

---
+++ js/hello-asteroid.js
@@ -0,0 +1,15 @@
+// console.log("My favorite food to eat is Seafood Enchiladas from Meso Maya. " + "I like the shrimp, crab and the sauce they use.");
+
+var favoriteFood = "Seafood Enchiladas"
+
+var restauraunt = "Meso Maya"
+
+var iLike = "shrimp, crab and the salsa"
+
+
+
+var phrase1 = `My favorite food to eat is ${favoriteFood} from ${restauraunt}. `
+
+var phrase2 = `I like the ${iLike} they use.`
+
+console.log( phrase1 + phrase2 );
