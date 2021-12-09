94aed04dd25fca56a1d848fb2a1b79e8cfc265bf
Irvin Ruiz <ruizirvin084@gmail.com>
2021-12-09 14:04:36 -0600

    worked on asteroid file and dataTypesAndOperators 2pm

--- js/dataTypesOperatorsVariables.js
+++ js/dataTypesOperatorsVariables.js
@@ -1,7 +1,14 @@
 let a = 1;
 let b = a++;
 let c = ++a;
+
 // what is the value of a, b, and c?
+
+console.log(a);
+
+console.log(b);
+
+console.log(c);
 
 console.log(a + b + c);
 
@@ -57,6 +64,9 @@ console.log(isNaN("false"));
 console.log(isNaN(false));
 
 // to illustrate why the isNaN() function is needed:
+
+//  //
+
 NaN == NaN
 
 !true
@@ -87,6 +97,8 @@ NaN == NaN
 
 !!"0"
 
+// 2.
+
 let sample = "Hello Codeup ";
 
 console.log(sample.length);
@@ -101,5 +113,85 @@ console.log(student.indexOf("c"));
 
 console.log(student.indexOf("C"));
 
-console.log(sample.substr(6, 6))
+console.log(sample.substr(6, 6));
 
+// 3.
+
+let movieRentalRate = 3;
+
+let mermaidDays = 3;
+
+let brotherBearDays = 5;
+
+let herculesDays = 1;
+
+let totalDays = mermaidDays + brotherBearDays + herculesDays;
+
+let totalPrice = totalDays * movieRentalRate;
+
+console.log("Total price for the rental movies is: $" + totalPrice);
+
+// 4.
+
+let username = 'codeup';
+let password = 'notastrongpassword';
+
+let maxPswdLength = true; // valid if true
+
+let usernameInPassword = true; // not valid if true
+
+let maxUsernameLength = true; // valid if true
+
+let usernameWhitespace = true; // not valid if true
+
+let pswdWhitespace = true; // not valid if true
+
+
+// check Password Length greater than or equal to 5
+if (password.length >= 5){
+    maxPswdLength = true;
+    console.log("Password length is valid");
+} else {
+    maxPswdLength = false;
+    console.log("Password length is NOT valid. Must be greater than 4 characters!");
+}
+
+// checks if password includes username
+
+if (password.includes(username)){
+    usernameInPassword = true;
+    console.log("Password cannot include username!");
+} else {
+    usernameInPassword = false;
+    console.log("Password is free of username");
+}
+
+// checks if username less than 21 char
+
+if (username.length <= 20){
+    maxUsernameLength = true;
+    console.log("Username length is valid");
+} else {
+    maxUsernameLength = false;
+    console.log("Please use less than 21 characters for your username!");
+}
+
+// checks if username contains whitespaces
+
+if (username.includes(" ")){
+    usernameWhitespace = true;
+    console.log("No whitespaces in username allowed");
+} else {
+    usernameWhitespace = false;
+    console.log("Username is free of whitespaces!");
+}
+
+// checks if password contains whitespaces
+
+if (password.includes(" ")){
+    pswdWhitespace = true;
+    console.log("No whitespaces in password allowed");
+} else {
+    pswdWhitespace = false;
+    console.log("Password is free of white spaces");
+}
--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -13,4 +13,3 @@ let phrase1 = `\n My\n favorite\n food\n to\n eat\n is\n ${favoriteFood}\n from\
 let phrase2 = `\n\tI\n \tlike\n \tthe\n \t${whatILikeAboutIt}\n \tthey\n \tuse.`
 
 console.log( phrase1 + phrase2 );
-
