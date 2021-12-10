14c25e4d2fdced8a9c7520ee75e21a3512f9acf4
Irvin Ruiz <ruizirvin084@gmail.com>
2021-12-10 09:36:10 -0600

    added template litteral to warmup.js

--- js/warmup.js
+++ js/warmup.js
@@ -1,11 +1,17 @@
 
-let creditScore = 650;
-let cashOnHand = 11000;
 
-if (creditScore > 680 && cashOnHand >= 4000){ // checks credit score above 680 and C.O.H above 9999
-    console.log("You are qualified");
-} else if (cashOnHand >= 10000){               // no good credit, check if at least $10,000
-    console.log("You are qualified");
-} else {                                       // no credit no down
-    console.log("You are not qualified");
+// Their credit score must be over 680 AND have at least $4000 on-hand
+// OR, they must have at least $10000 on-hand
+
+let creditScore = 650;
+let cashOnHand = 5000;
+
+let result = "\nApplication Results\nYour application was : "
+
+if (creditScore > 680 && cashOnHand >= 4000) {  // checks credit score above 680 and C.O.H above 9999
+    console.log(`${result}Approved`);
+} else if (cashOnHand >= 10000) {               // no good credit, check if at least $10,000
+    console.log(`${result}Approved`);
+} else {                                        // no credit & no down = no loan
+    console.log(`${result}Not Approved`);
 }
