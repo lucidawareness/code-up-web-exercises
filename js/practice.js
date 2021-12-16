e2472cda00a77ddb0ab77b54555582c89d994aca
Irvin Ruiz <ruizirvin084@gmail.com>
2021-12-16 15:35:38 -0600

    Finished while.js added practice.js

---
+++ js/practice.js
@@ -0,0 +1,12 @@
+"use strict";
+(function () {
+	// // TODO: MINI EXERCISE
+	// // TODO: print all even numbers between 0 and 25;
+	let counter = 0; // starting point
+
+	while (counter < 25) {
+		console.log(counter)
+		counter +=2;
+	}
+
+})();
