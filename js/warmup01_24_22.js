8fb6444c7fac819bd4479fec1f067ddf98195ce9
Irvin Ruiz <ruizirvin084@gmail.com>
2022-01-24 09:16:05 -0600

    completed js warmup 01/24/22

--- js/warmup01_24_22.js
+++ js/warmup01_24_22.js
@@ -1,6 +1,19 @@
 "use strict";
 (function () {
 
+	function reverseString(str) {
+		if (typeof str === "string") {
+			let splitString = str.split("");
+
+			let reverseArray = splitString.reverse();
+
+			return reverseArray.join("");
+		}
+		return "Please input a string!";
+	}
+
+	console.log(reverseString("hello"));
+	console.log(reverseString(65465));
 
 
 })();
